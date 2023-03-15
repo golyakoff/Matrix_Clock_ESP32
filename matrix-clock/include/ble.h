8202b6283dedb1bbda310f8e42ad5bbc52f6e68a
Andrei Golyakov <golyakoff@yandex.ru>
2023-03-16 01:38:59 +0300

    BLE Date Time Service read chars added

--- matrix-clock/include/ble.h
+++ matrix-clock/include/ble.h
@@ -4,10 +4,58 @@
 #include "time.h"
 
 //BLE server name
-#define BLE_SERVER_NAME "Tetris Clock A1"
+#define BLE_SERVER_NAME                     "Tetris Clock A1"
 
-#define SERVICE_UUID "91bad492-b950-4226-aa2b-4ede9fa42f59"
-#define TIME_CHAR_UUID "ca73b3ba-39f6-4ab3-91ae-186dc9577d99"
+#define DT_SERVICE_UUID                     "00001847-0000-1000-8000-00805F9B34FB"  // 0x1847
+
+#define DEVICE_TIME_FEATURE_CHAR_UUID       "00002B8E-0000-1000-8000-00805F9B34FB"  // 0x2B8E M Read
+#define DEVICE_TIME_PARAMETERS_CHAR_UUID    "00002B8F-0000-1000-8000-00805F9B34FB"  // 0x2B8F M Read
+#define DEVICE_TIME_CHAR_UUID               "00002B90-0000-1000-8000-00805F9B34FB"  // 0x2B90 M Read, Indicate
+#define DEVICE_TIME_CONTROL_POINT_CHAR_UUID "00002B91-0000-1000-8000-00805F9B34FB"  // 0x2B91 M Write, Indicate
+
+// 0x2B92 Mandatory if the Time Change Logging feature is supported, otherwise Excluded Notify
+// #define TIME_CHANGE_LOG_DATA_CHAR_UUID      "00002B92-0000-1000-8000-00805F9B34FB"
+
+typedef enum
+{
+    // 0 : E2E-CRC. E2E_CRC field implemented in each characteristic within the service. O.
+    DTF_E2E_CRC                     = 1 <<  0,
+    // 1 : Time Change Logging. Time change logging is implemented to capture and preserve details of time change events O.
+    DTF_TIME_CHANGE_LOGGING         = 1 <<  1,
+    // 2 : Base Time Second-Fractions. Time values include Base Time fractions of a second
+    //     (16-bit fractions of a second; 1/65,536 Second). O.
+    DTF_BASE_SECOND_FRACTIONS       = 1 <<  2,
+    // 3 : Time or Date Displayed to User. The device displays either time or date values or both. O.
+    DTF_TIME_OR_DATE_DISPLAY        = 1 <<  3,
+    // 4 : Displayed Formats. Formatting of the device’s displayed date and time is revealed using the DT Parameters characteristic. C1.
+    DTF_DISPLAYED_FORMATS           = 1 <<  4,
+    // 5 : Displayed Formats Changeable. The device can change the format of the displayed date or time and the Server 
+    //     can indicate these format changes. C2.
+    DTF_DISPLAYED_FORMAT_CHANGEABLE = 1 <<  5,
+    // 6 : Separate User Timeline. The device supports allowing a user to set the time or date of the device (“User-facing Time”),
+    //     creating a separate timeline from the synchronized Base Time. O.
+    DTF_SEPARATE_USER_TIMELINE      = 1 <<  6,
+    // 7 : Authorization Required. Authorization required to complete certain DTCP procedures. O.
+    DTF_AUTHORIZATION_REQUIRED      = 1 <<  7,
+    // 8 : RTC Drift Tracking. The Server supports monitoring RTC drift after being synchronized to a time source. O.
+    DTF_RTC_DRIFT_TRACKING          = 1 <<  8,
+    // 9 : Epoch Year 1900. The Server supports reporting and receiving Time Update procedures where the Base Time is based on Epoch 1900. C3.
+    DTF_EPOCH_YEAR_1900             = 1 <<  9,
+    // A : Epoch Year 2000. The Server supports reporting and receiving Time Update procedures where the Base Time is based on Epoch 2000. C3.
+    DTF_EPOCH_YEAR_2000             = 1 << 10,
+    // B : Propose Non-Logged Time Adjustment Limit. The Server supports changes to the DT Parameters field Non_Logged_Time_Adjustment_Limit using the DTCP. O.
+    DTF_NON_LOGGED_TIME_ADJ_LIMIT   = 1 << 11,
+    // C : Retrieve Active Time Adjustments. The Server supports the retrieval of either non-logged 
+    //     Base_Time adjustments or consolidated Base_Time adjustments or both by using the DTCP to resolve a
+    //     Base_Time value discrepancy. O.
+    DTF_RETRIVE_ACT_TIME_ADJ        = 1 << 12
+    // D-F: Reserved for Future Use.
+} dtf_t;
+
+struct dtp_t {
+    uint16_t rtc_resolution;
+    uint16_t dt_format;
+};
 
 void ble_init();
 void ble_update_time(struct tm *dt);
--- matrix-clock/src/ble.cpp
+++ matrix-clock/src/ble.cpp
@@ -3,31 +3,70 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include <string.h>
 
 #include "ble.h"
 
-#define SERVICE_UUID "91bad492-b950-4226-aa2b-4ede9fa42f59"
+// BLE Transmission Type: LSO: Least Significant Octet First
 
-BLECharacteristic timeCharacteristics(TIME_CHAR_UUID, BLECharacteristic::PROPERTY_NOTIFY);
-BLEDescriptor timeDescriptor(BLEUUID((uint16_t)0x2903));
+// Declare and init BLE characteristics
+BLECharacteristic _dtfChar(DEVICE_TIME_FEATURE_CHAR_UUID, BLECharacteristic::PROPERTY_READ);
+BLECharacteristic _dtpChar(DEVICE_TIME_PARAMETERS_CHAR_UUID, BLECharacteristic::PROPERTY_READ);
+BLECharacteristic _dtChar(DEVICE_TIME_CHAR_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic _dtcpChar(DEVICE_TIME_CONTROL_POINT_CHAR_UUID, BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
 
-bool deviceConnected = true;
+// Declare and init BLE descriptor
+BLEDescriptor _dtDescriptor(BLEUUID((uint16_t)0x2903));
+
+// Date Time Features characteristic value
+uint16_t _dtf = (uint16_t)DTF_EPOCH_YEAR_1900 | (uint16_t)DTF_DISPLAYED_FORMATS;
+
+// Date Time Properties characteristic value
+// Displayed Date Format (LSO) 0x07 = YYYY.MM.DD ("2016.12.31")
+// Displayed Time Format (MSO – Lower nibble) 1110b = 24h fixed length with seconds ("09:05:42", "22:15:26")
+// Displayed Date Field Separator (MSO – Upper nibble) 0010b = Hyphen ("31-Dec-2016")
+struct dtp_t _dtp = { 
+    .rtc_resolution = 0xffff,
+    .dt_format = ((uint16_t)0b0010 << 12) | ((uint16_t)0b1110 << 8) | (uint16_t)0x07
+};
+
+// Device connected state
+bool _deviceConnected = true;
+
+// Temporary variables
+struct tm _lastUpdatedDateTime;
+char _dtValue[20];
+std::string _str_dtValue;
 
 class MyServerCallbacks: public BLEServerCallbacks
 {
     void onConnect(BLEServer* pServer) 
     {
-        deviceConnected = true;
+        _deviceConnected = true;
+        Serial.println("BLE device connected");
     }
     
     void onDisconnect(BLEServer* pServer)
     {
-        deviceConnected = false;
+        _deviceConnected = false;
+        Serial.println("BLE device disconnected");
+
+        // Start advertising
+        Serial.print("Start advertising... ");
+        BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+        pAdvertising->addServiceUUID(DT_SERVICE_UUID);
+        pServer->getAdvertising()->start();
+        Serial.println("OK");
     }
 };
 
+bool dtAreDifferent(struct tm *dt1, struct tm *dt2);
+
 void ble_init()
 {
+    // reset last updated date and time
+    memset(&_lastUpdatedDateTime, 0, sizeof(_lastUpdatedDateTime));
+
     // Create the BLE Device
     Serial.print("BLEDevice::init(BLE_SERVER_NAME)... ");
     BLEDevice::init(BLE_SERVER_NAME);
@@ -43,15 +82,27 @@ void ble_init()
     Serial.println("OK");
 
     // Start a BLE service with the service UUID.
-    Serial.print("BLEService *bleService = pServer->createService(SERVICE_UUID)... ");
-    BLEService *bleService = pServer->createService(SERVICE_UUID);
+    Serial.print("BLEService *bleService = pServer->createService(DT_SERVICE_UUID)... ");
+    BLEService *bleService = pServer->createService(DT_SERVICE_UUID);
     Serial.println("OK");
 
-    // Add time characteristic
-    Serial.print("Add time characteristic... ");
-    bleService->addCharacteristic(&timeCharacteristics);
-    timeDescriptor.setValue("Current Date & Time");
-    timeCharacteristics.addDescriptor(new BLE2902());
+    // Add Device Time Feature characteristic
+    Serial.print("Add Device Time Feature characteristic... ");
+    bleService->addCharacteristic(&_dtfChar);
+    _dtfChar.setValue((uint8_t*)(&_dtf), sizeof(_dtf));
+    Serial.println("OK");
+
+    // Add Device Time Parameters characteristic
+    Serial.print("Add Device Time Parameters characteristic... ");
+    bleService->addCharacteristic(&_dtpChar);
+    _dtpChar.setValue((uint8_t*)(&_dtp), sizeof(_dtp));
+    Serial.println("OK");
+
+    // Add Device Time Parameters characteristic
+    Serial.print("Add Device Time characteristic... ");
+    bleService->addCharacteristic(&_dtChar);
+    _dtDescriptor.setValue("Device Time");
+    _dtChar.addDescriptor(new BLE2902());
     Serial.println("OK");
 
     // Start the service
@@ -62,16 +113,38 @@ void ble_init()
     // Start advertising
     Serial.print("Start advertising... ");
     BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->addServiceUUID(DT_SERVICE_UUID);
     pServer->getAdvertising()->start();
     Serial.println("OK");
 }
 
 void ble_update_time(struct tm *dt)
 {
-    if (deviceConnected) {
+    if (_deviceConnected && dtAreDifferent(dt, &_lastUpdatedDateTime)) {
+        memcpy(&_lastUpdatedDateTime, dt, sizeof(struct tm));
         //Set temperature Characteristic value and notify connected client
-        timeCharacteristics.setValue(dt->tm_sec);
-        timeCharacteristics.notify();
+        sprintf(
+            _dtValue,
+            "%d-%02d-%02d %02d:%02d:%02d",
+            1900 + dt->tm_year,
+            dt->tm_mon,
+            dt->tm_mday,
+            dt->tm_hour,
+            dt->tm_min,
+            dt->tm_sec);
+        
+        _str_dtValue.assign(_dtValue, sizeof(_dtValue));
+        _dtChar.setValue(_str_dtValue);
+        _dtChar.notify();
     }
+}
+
+bool dtAreDifferent(struct tm *dt1, struct tm *dt2)
+{
+    return dt1->tm_sec  != dt2->tm_sec
+        || dt1->tm_min  != dt2->tm_min
+        || dt1->tm_hour != dt2->tm_hour
+        || dt1->tm_mday != dt2->tm_mday
+        || dt1->tm_mon  != dt2->tm_mon
+        || dt1->tm_year != dt2->tm_year;
 }
