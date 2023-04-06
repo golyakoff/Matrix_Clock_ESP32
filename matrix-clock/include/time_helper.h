fffea5d07cdf489bbb302381928777338f9409e0
Andrei Golyakov <golyakoff@yandex.ru>
2023-04-06 23:47:49 +0300

    #ifndef > #pragma once

--- matrix-clock/include/time_helper.h
+++ matrix-clock/include/time_helper.h
@@ -1,11 +1,7 @@
-#ifndef __TIME_HELPER_H__
-#define __TIME_HELPER_H__
+#pragma once
 
 #include "time.h"
 #include "stdbool.h"
 
 bool times_are_different(const struct tm* dt1, const struct tm* dt2);
 void println_tm(const char *prefix, const struct tm *dt);
-
-#endif // __TIME_HELPER_H__
-
